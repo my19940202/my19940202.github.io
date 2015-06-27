fda39356086ea48dafab251917cd9928f99bd28b
xishengbo <xishengbo@baidu.com>
2015-06-27 17:50:51 +0800

    添加新的文章

--- xsb/js/article.js
+++ xsb/js/article.js
@@ -1,15 +1,17 @@
 var dataList = {
 	_js:[
-		{title:'JS性能优化1', date:'2015-06-27', curl:'./article.html#1'}
+		{title:'JS性能优化1', date:'2015-06-27', curl:'./article.html#1'},
+		{title:'JS元算符优先级', date:'2015-06-27 16:32', curl:'./article.html#priority'}
+
 	],
 	_linux:[
 		{title:'linux这是第1篇文章', date:'2015-06-28', curl:'./article.html#12'}
 	],
 	_ios:[
-		{title:'ios这是第1篇文章', date:'2015-06-28', curl:'./article.html#12'}
+		{title:'寒假时的ios记录', date:'2015-06-28', curl:'./article.html#ios1'}
 	],
 	_css:[
-		{title:'css这是第1篇文章', date:'2015-06-28', curl:'./article.html#12'}
+		{title:'resetcss', date:'2015-06-28', curl:'./article.html#resetcss'}
 	],
 	_else:[
 		{title:'杂记', date:'2015-06-27', curl:'./article.html#2'}
