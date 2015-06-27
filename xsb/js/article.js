3a4f85ea3d6f632e7be4585fb3dbd6527c5a4013
xishengbo <xishengbo@baidu.com>
2015-06-27 11:51:13 +0800

    静态js大部分已经完成

---
+++ xsb/js/article.js
@@ -0,0 +1,20 @@
+var dataList = {
+	_js:[
+		{title:'这是第一篇文章', date:'2015-06-27', curl:'./article.html#1'},
+		{title:'这是第二篇文章', date:'2015-06-27', curl:'./article.html#2'},
+		{title:'这是第4篇文章', date:'2015-06-23', curl:'./article.html#3'},
+		{title:'这是第三篇文章', date:'2015-06-28', curl:'./article.html#12'}
+	],
+	_linux:[
+		{title:'linux这是第1篇文章', date:'2015-06-28', curl:'./article.html#12'}
+	],
+	_ios:[
+		{title:'ios这是第1篇文章', date:'2015-06-28', curl:'./article.html#12'}
+	],
+	_css:[
+		{title:'css这是第1篇文章', date:'2015-06-28', curl:'./article.html#12'}
+	],
+	_else:[
+		{title:'else这是第1篇文章', date:'2015-06-28', curl:'./article.html#12'}
+	]
+};
