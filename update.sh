664ee5aba8110b2a9518f3e994550370523b5eae
xibobo <xishengbo@baidu.com>
2018-10-16 10:28:45 +0800

    update commit shell

--- update.sh
+++ update.sh
@@ -3,4 +3,4 @@ cd /Users/baidu/GitBook/Library/my19940202/xsb_dev_manual/ && gitbook build
 cd -
 cp -r /Users/baidu/GitBook/Library/my19940202/xsb_dev_manual/_book/* .
 git add *
-git commit -m msg
+git commit -m $msg
