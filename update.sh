907ed073603c6658c0e2fdd7a01a25bc3ce84a6d
xibobo <xishengbo@baidu.com>
2018-10-16 10:27:44 +0800

    msg

--- update.sh
+++ update.sh
@@ -1,4 +1,6 @@
+msg=$1
 cd /Users/baidu/GitBook/Library/my19940202/xsb_dev_manual/ && gitbook build
 cd -
 cp -r /Users/baidu/GitBook/Library/my19940202/xsb_dev_manual/_book/* .
-# git commit -m "update page content"
+git add *
+git commit -m msg
